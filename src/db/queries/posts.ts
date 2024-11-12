4482ed4a8ee976086944b378a4a34a001bd9c1f6
neekyo <codebyns@gmail.com>
2024-11-12 15:36:28 -0500

    comments

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -9,6 +9,10 @@ export type PostWithData = (
   }
 )
 
+// export type PostWithData = Awaited<
+// ReturnType<typeof fetchPostsByTopicSlug>
+// >[number];
+
 export function fetchPostsByTopicSlug(slug: string):Promise<PostWithData[]> {
   return db.post.findMany({
     where: { topic: { slug }},
