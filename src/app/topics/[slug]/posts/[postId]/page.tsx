922ee394c05a6c10d3815ee20a0d34afb71fe103
neekyo <codebyns@gmail.com>
2024-11-13 11:11:58 -0500

    before migration to postgresql

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -7,14 +7,15 @@ import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
 
 interface PostShowPageProps {
-  params: {
+  params: Promise<{
     slug: string;
     postId: string;
-  };
+  }>;
 }
 
 export default async function PostShowPage({ params }: PostShowPageProps) {
   const { slug, postId } = await params;
+
   return (
     <div className="space-y-3">
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
